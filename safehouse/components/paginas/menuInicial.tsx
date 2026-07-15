4fa997f61868bfd7cd38be7aedef1e22675cd2ed
João Marcos de Freitas <joaofreitas.souza@ucsal.edu.br>
2026-07-15 03:35:10 -0300

    Foi implementado algumas das funcionalidades de conexão com o backend, testado e confirmado. A conexão existe está ativa localmente com persistência. Ainda falta terminar as integrações com o sistema e testar as funcionalidades implementadas. O registro de pessoa funciona e a busca de todas as pessoas também.

--- safehouse/components/paginas/menuInicial.tsx
+++ safehouse/components/paginas/menuInicial.tsx
@@ -2,17 +2,43 @@
 import { CartaoPessoaPrevia } from "../ui/cartaoPessoaPrevia";
 //importa a prévia do cartão de transação
 import { CartaoTransacaoPrevia } from "../ui/cartaoTransacaoPrevia";
+//indica para o react que a tela terá memória e interação, além de garantir o necessário para realizar requisições
+import { useState, useEffect } from "react";
 
 //componente da tela de menu inicial
 export function MenuInicial({ aoClicar }: { aoClicar: (botaoClicado: string) => void }) {
+    //define variaveis para trahalhar com a memoria da pagina. pessoas é o valor que queremo usar para manipular dados, e definirPessoas é quem define o valor de pessoas.
+    const [ pessoas, definirPessoas ] = useState("");
+    const [ erroPessoas, definirErroPessoas ] = useState("");
 
-    //lógica para busca de nomes no BD.
-        
+    //use effect vai buscar apenas uma vez quando a página for carregada
+    useEffect(() => {
+        //URL do backend para requisição
+        fetch("http://localhost:5004/api/pessoa").then(resposta => {
+            //verifica se a resposta deu errado
+            if (!resposta.ok) {
+                //joga um erro sobre a falha na busca
+                throw new Error("Não foi possível buscar as pessoas");
+            }
+
+            //retorna a resposta em JSON.
+            return resposta.json();
+        }).then(dados => {
+            //salva os dados buscados quando der ccerto
+            definirPessoas(dados);
+        }).catch(erro => {
+            //trata erros de conexão e outras falhas
+            definirErroPessoas(erro);
+        });
+    }, []); // o colchete vazio indica que será rodado apenas uma vez.
+
     return(
         <div className="flex flex-row justify-center p-[5vh] gap-[10%] h-[100vh]">
             <div className="flex flex-col items-center">
                 <h1 className="font-[Josefin_Sans] text-[25px] text-white font-bold tracking-[0.085rem]">Pessoas:</h1>
-                <CartaoPessoaPrevia nome="Tony Stark" idade={45}/>
+                {
+                    
+                }
                 <h3 onClick={() => aoClicar("todasPessoas")} className="font-[Josefin_Sans] text-[16px] text-white font-bold tracking-[0.085rem] mt-[20%] cursor-pointer"><u>Ver Tudo</u></h3>
             </div>
 
--- safehouse/components/paginas/registrarPessoa.tsx
+++ safehouse/components/paginas/registrarPessoa.tsx
@@ -4,8 +4,14 @@ import { SubmitEvent } from "react";
 //importa botão de registro
 import { BotaoRegistrar } from "../ui/botaoRegistrar";
 
+//indica ao react que a página terá memória e interação, além de garantir o necessário para realizar requisições
+import { useState } from "react";
+
 //componente de página para registro de pessoa.
 export function RegistrarPessoa() {
+    //define variaveis para trahalhar com a memoria da pagina. pessoas é o valor que queremo usar para manipular dados, e definirPessoas é quem define o valor de pessoas.
+    const [ pessoaErro, definirPessoaErro ] = useState("");
+
     //função para lidar com o envio do formulário. 'e' é o evento que é uma variável do tipo SubmitEvent, o tipo genérico é usado para especificar como SubmitEvent vai definir seus parâmetros, ex: só é possível usar e.currentTarget, quando definimos que SubmitEvent tem o tipo genérico HTMLFormElement (ou seja é um formulário que estamos lidando)
     const respostaForm = (e: SubmitEvent<HTMLFormElement>) => {
         //impede formulário de recarregar a página ao ser enviado
@@ -16,6 +22,24 @@ export function RegistrarPessoa() {
 
         const nome = dados.get("nome") || "";
         const idade = dados.get("idade") || "";
+
+        const pessoaNova = {
+            "nome": nome,
+            "idade": idade
+        }
+
+        //URL do backend para requisição
+        fetch("http://localhost:5004/api/pessoa", {
+            //define o metodo POST
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(pessoaNova)
+        }).catch(erro => {
+            //salva resposta de erro da requisição
+            definirPessoaErro(erro);
+        });
     }
 
     return(
@@ -28,6 +52,8 @@ export function RegistrarPessoa() {
                 <input type="number" name="idade" min={1} max={120} className="font-[Josefin_Sans] text-[18px] text-white font-light tracking-[0.085rem] w-[40%] pl-[5%] border-b-[1px]" placeholder="Idade"/>   
                 <BotaoRegistrar/>
             </form> 
+
+            <h1 className="font-[Josefin_Sans] text-[25px] text-white font-bold tracking-[0.085rem]">{pessoaErro}</h1>
         </div>
     );
 }
